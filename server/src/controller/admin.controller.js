2fd732a8260d194b822251a58c615fe0e09667bc
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-20 00:57:00 +0700

    Done

--- server/src/controller/admin.controller.js
+++ server/src/controller/admin.controller.js
@@ -1,3 +1,111 @@
-export const getAdmin = (req, res) => {
-  res.send("Admin route with GET method");
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
+import cloudinary from "../lib/cloudinary.js";
+//helper function for cloudinary upload
+const uploadToCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      resource_type: "auto",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.log("Error in uploadToCloudinary", error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
+export const createSong = async (req, res, next) => {
+  try {
+    //Neu tat ca nhung file nay khong co thi tra ve 400
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
+      return res.status(400).json({ message: "Please upload all files" });
+    }
+    const { title, artist, albumId, duration } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+
+    const audioUrl = await uploadToCloudinary(audioFile);
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    const song = new Song({
+      title,
+      artist,
+      audioUrl,
+      imageUrl,
+      duration,
+      albumId: albumId || null,
+    });
+
+    await song.save();
+
+    //neu song thuoc album thi cap nhat them thong tin song do vao album
+    if (albumId) {
+      await Album.findByIdAndUpdate(albumId, {
+        $push: { songs: song._id },
+      });
+    }
+
+    res.status(200).json(song);
+  } catch (error) {
+    console.log("Error in createSong", error);
+    next(error);
+  }
+};
+export const deleteSong = async () => {
+  try {
+    const { id } = req.params;
+    const song = await Song.findById(id);
+
+    //neu song nam trong album thi cap nhap lai album
+    if (song.albumId) {
+      await Album.findByIdAndUpdate(song.albumId, {
+        $pull: { songs: song._id },
+      });
+    }
+
+    await Song.findOneAndDelete(id);
+
+    res.status(200).json({ message: "Song deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteSong", error);
+    next(error);
+  }
+};
+
+export const createAlbum = async (req, res, next) => {
+  try {
+    const { title, artist, releaseYear } = req.body;
+
+    const { imageFile } = req.file;
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    const album = new Album({
+      title,
+      artist,
+      imageUrl,
+      releaseYear,
+    });
+
+    await album.save();
+
+    res.status(201).json(album);
+  } catch (error) {
+    console.log("Error in createAlbum", error);
+    next(error);
+  }
+};
+
+export const deleteAlbum = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Song.deleteMany({ albumId: id });
+    await Album.findByIdAndDelete(id);
+    res.status(200).json({ message: "Album deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteAlbum", error);
+    next(error);
+  }
+};
+
+export const checkAdmin = async (req, res, next) => {
+  res.status(200).json({ admin: true });
 };
