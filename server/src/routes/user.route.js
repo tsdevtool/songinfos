7291abb8e160398fcf4f8846284502da0b80fad9
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-19 21:03:43 +0700

    Done

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", protectRoute, (req, res) => {
+  // req.auth.userId;
   res.send("User route with GET method");
 });
 
