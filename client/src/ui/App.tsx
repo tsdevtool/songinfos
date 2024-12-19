32f490725aa6aff1724737f0ee469c1e03779f0e
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-19 19:09:18 +0700

    backendsetup

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -1,14 +1,23 @@
 
 import { Button } from '@/components/ui/button'
-import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-red-600 text-5xl'>Spotify</h1>
-      <Button variant={"outline"}>This is a button</Button>
-      <h2>Nguyen Thanh SIeu</h2>
+      <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
   )
 }
