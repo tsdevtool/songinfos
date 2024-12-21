d11cd98345ba889dc2bda45c2fc3c5ac8be72359
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-21 17:36:51 +0700

    Done

--- server/src/routes/stats.route.js
+++ server/src/routes/stats.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-
+import { getStats } from "../controller/stat.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 const router = Router();
 
-router.get("/", (req, res) => {});
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
